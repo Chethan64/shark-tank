2f3f40ce4eb27ef2cf1e433b088f4663505c2112
Chethan64 <padma.chethan@gmail.com>
2022-04-12 03:18:32 +0530

    Connected to local database - mongodb

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import json
 from flask import Flask, jsonify, request, Response
+from database.db import client
 
 app = Flask(__name__)
-
-pitches = list()
+database = client.xharktank
+collection = database.pitches
 
 @app.route('/')
 def root():
@@ -14,37 +15,43 @@ def root():
 @app.route('/pitches', methods=['POST'])
 def post_pitch():
     body = request.get_json()
+    
+    id_ = collection.count_documents({}) + 1
     body["offers"] = list()
-    pitches.append(body)
-    response = {"id": len(pitches)}
+    body["id"] = id_
+    collection.insert_one(body)
+
+    response = {"id": id_}
     response = json.dumps(response)
     return Response(response=response, mimetype='application/json', status=201)
 
 @app.route('/pitches', methods=['GET'])
 def get_pitches():
-    print(pitches)
-    print(type(pitches))
-    rev = pitches[::-1]
-    response = json.dumps(rev)
+    response = []    
+    for document in collection.find({}, {'_id': 0}):
+        response.insert(0, document)
+    response = json.dumps(response)
     return Response(response=response, mimetype='application/json', status=200)
 
 @app.route('/pitches/<int:id>/makeOffer', methods=['POST'])
 def make_offer(id):
-    if(id > len(pitches)):
+    if(id > collection.count_documents({})):
         return Response(status=404)
         
     body = request.get_json()
-    pitches[id-1]["offers"].append(body)
-    response = {"id": len(pitches[id-1]["offers"])}
-    response = json.dumps(response)
+    document = collection.find_one({"id": id})
+    offer_id = len(document['offers']) + 1
+    body["id"] = offer_id
+    collection.update_one({'id': id}, {'$push': {'offers': body}})
+    response = json.dumps({'id': offer_id})
     return Response(response=response, mimetype='application/json', status=201)
 
 @app.route('/pitches/<int:id>', methods=['GET'])
 def get_pitch(id):
-    if(id > len(pitches)):
+    if(id > collection.count_documents({})):
         return Response(status=404)
     
-    response = pitches[id-1]
+    response = collection.find_one({"id": id}, {'_id': 0})
     response = json.dumps(response)
     return Response(response=response, mimetype='application/json', status=200)
 
